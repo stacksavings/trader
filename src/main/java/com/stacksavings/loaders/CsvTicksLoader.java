258f3d35ec16e67ffcddb614773ca0a2d5ce9b33
juliopaulo <juliopaulo@gmail.com>
2017-07-25 23:30:40 -0400

    ROCIndicator Test

--- src/main/java/com/stacksavings/loaders/CsvTicksLoader.java
+++ src/main/java/com/stacksavings/loaders/CsvTicksLoader.java
@@ -18,7 +18,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.opencsv.CSVReader;
-import com.stacksavings.client.api.PoloniexClientApi;
 import com.stacksavings.utils.Constants;
 import com.stacksavings.utils.PropertiesUtil;
 
@@ -98,15 +97,47 @@ public class CsvTicksLoader {
 
         return new TimeSeries("apple_ticks", ticks);
     }
+    
+    public TimeSeries loadSeriesByFileName(String fileName) {
+    	
+    	String directoryPath = propertiesUtil.getProps().getProperty("path.directory");
+		//String fileName = propertiesUtil.getProps().getProperty("filename");
+		String filenameExtension = propertiesUtil.getProps().getProperty("filename.extension");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat(Constants.YYYY_MM_DD);
+		
+		Date dateNow= new Date();
+		
+		String sDateNow = sdf.format(dateNow);
+		
+		File file = new File(fileName);
+		
+		List<Tick> ticks = new ArrayList<>();
+		
+        try {
+        	
+        	InputStream stream = new FileInputStream(file); 
 
-    public static void main(String[] args) {
-        TimeSeries series = CsvTicksLoader.getInstance().loadSeries();
+            CSVReader csvReader = new CSVReader(new InputStreamReader(stream, Charset.forName("UTF-8")), ',', '"', 1);
+        	
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+                ZonedDateTime date = LocalDate.parse(line[0], DATE_FORMAT).atStartOfDay(ZoneId.systemDefault());
+                double open = Double.parseDouble(line[1]);
+                double high = Double.parseDouble(line[2]);
+                double low = Double.parseDouble(line[3]);
+                double close = Double.parseDouble(line[4]);
+                double volume = Double.parseDouble(line[5]);
 
-        System.out.println("Series: " + series.getName() + " (" + series.getSeriesPeriodDescription() + ")");
-        System.out.println("Number of ticks: " + series.getTickCount());
-        System.out.println("First tick: \n"
-                + "\tVolume: " + series.getTick(0).getVolume() + "\n"
-                + "\tOpen price: " + series.getTick(0).getOpenPrice()+ "\n"
-                + "\tClose price: " + series.getTick(0).getClosePrice());
+                ticks.add(new Tick(date, open, high, low, close, volume));
+            }
+        } catch (IOException ioe) {
+            Logger.getLogger(CsvTicksLoader.class.getName()).log(Level.SEVERE, "Unable to load ticks from CSV", ioe);
+        } catch (NumberFormatException nfe) {
+            Logger.getLogger(CsvTicksLoader.class.getName()).log(Level.SEVERE, "Error while parsing value", nfe);
+        }
+
+        return new TimeSeries("apple_ticks", ticks);
     }
+
 }
---
+++ src/test/java/com/stacksavings/ROCIndicatorTest.java
@@ -0,0 +1,48 @@
+package com.stacksavings;
+
+import static eu.verdelhan.ta4j.TATestsUtils.assertDecimalEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.stacksavings.loaders.CsvTicksLoader;
+
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.ROCIndicator;
+
+/**
+ * 
+ * @author julio paulo
+ *
+ */
+public class ROCIndicatorTest {
+
+    private ClosePriceIndicator closePrice;
+
+    @Before
+    public void setUp() {
+    	
+    	TimeSeries series = CsvTicksLoader.getInstance().loadSeriesByFileName("src//test//resources//files//chart_data_ROC.csv");
+    	
+        closePrice = new ClosePriceIndicator(series);
+    }
+
+    @Test
+    public void getValueWhenTimeFrameIs12() {
+    	
+        ROCIndicator roc = new ROCIndicator(closePrice, 12);
+
+        // Incomplete time frame
+        assertDecimalEquals(roc.getValue(0), 0);
+        assertDecimalEquals(roc.getValue(1), 1.105);
+        assertDecimalEquals(roc.getValue(2), -0.3319);
+        assertDecimalEquals(roc.getValue(3), 0.9648);
+
+        // Complete time frame
+        double[] results13to20 = new double[] { -3.8488, -4.8489, -4.5206, -6.3439, -7.8592, -6.2083, -4.3131, -3.2434 };
+        for (int i = 0; i < results13to20.length; i++) {
+            assertDecimalEquals(roc.getValue(i + 12), results13to20[i]);
+        }
+    }
+}
