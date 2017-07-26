0f289a38fb16655b261b009f05c48000adf3b65b
juliopaulo <juliopaulo@gmail.com>
2017-07-25 23:58:25 -0400

    Test ROC

--- src/test/java/com/stacksavings/ROCIndicatorTest.java
+++ src/test/java/com/stacksavings/ROCIndicatorTest.java
@@ -41,17 +41,5 @@ public class ROCIndicatorTest {
         	System.out.println( roc.getValue(i) );
         }
         
-        // Incomplete time frame
-        /** assertDecimalEquals(roc.getValue(0), 0);
-        assertDecimalEquals(roc.getValue(1), 1.105);
-        assertDecimalEquals(roc.getValue(2), -0.3319);
-        assertDecimalEquals(roc.getValue(3), 0.9648);
-
-        // Complete time frame
-        double[] results13to20 = new double[] { -3.8488, -4.8489, -4.5206, -6.3439, -7.8592, -6.2083, -4.3131, -3.2434 };
-        for (int i = 0; i < results13to20.length; i++) {
-            assertDecimalEquals(roc.getValue(i + 12), results13to20[i]);
-        }
-        **/
     }
 }
