50f544b2fd69aedbb389fd319f8daa38a345c86e
juliopaulo <juliopaulo@gmail.com>
2017-08-08 23:25:16 -0400

    ROCIndicator with a lot of coins

--- src/main/java/com/stacksavings/indicators/ROCIndicatorCalculate.java
+++ src/main/java/com/stacksavings/indicators/ROCIndicatorCalculate.java
@@ -1,11 +1,15 @@
 package com.stacksavings.indicators;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.stacksavings.client.api.PoloniexClientApi;
 import com.stacksavings.loaders.CsvTicksLoader;
+import com.stacksavings.utils.Constants;
 import com.stacksavings.utils.FileManager;
+import com.stacksavings.utils.ROCIndicatorUtils;
 
+import eu.verdelhan.ta4j.Decimal;
 import eu.verdelhan.ta4j.TimeSeries;
 import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
 import eu.verdelhan.ta4j.indicators.trackers.ROCIndicator;
@@ -46,11 +50,26 @@ public class ROCIndicatorCalculate {
 		for (String currency : currencyPairList) 
 		{
 			String fileNameCurrencyPair = fileManager.getFileNameByCurrency(currency);
+			
 			TimeSeries series = csvTicksLoader.loadSeriesByFileName(fileNameCurrencyPair);
 			ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
 			
-			ROCIndicator roc = new ROCIndicator(closePrice, 12);
-
+			ROCIndicator roc = new ROCIndicator(closePrice, Constants.TIME_FRAME_12);
+			
+			final int nbTicks = series.getTickCount();
+	        
+	        List<Decimal> results =new ArrayList<Decimal>();
+	        
+	        for (int i = 0; i < nbTicks; i++) {
+	        	results.add(roc.getValue(i)) ;
+	        }
+	        
+	        List<Decimal> resultFinal =  ROCIndicatorUtils.calculateRisePrice(results);
+	        System.out.println(" BUY Signal for currency : "+currency);
+	        for (Decimal decimal : resultFinal) {
+				System.out.println(" BUY Signal : "+decimal);
+			}
+	        
 		}
 	}
 }
--- src/main/java/com/stacksavings/utils/Constants.java
+++ src/main/java/com/stacksavings/utils/Constants.java
@@ -10,19 +10,10 @@ public class Constants {
 	public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 	public static final String YYYY_MM_DD = "yyyy_MM_dd";
 	
-	// Currency pay available in poloniex api
 	
-	public static final String BTC_ETH = "BTC_ETH";
-	public static final String USDT_BTC = "USDT_BTC";
-	public static final String BTC_STR = "BTC_STR";
-	public static final String BTC_XRP = "BTC_XRP";
-	public static final String BTC_LTC = "BTC_LTC";
-	public static final String USDT_ETH = "USDT_ETH";
-	public static final String BTC_BTS = "BTC_BTS";
-	public static final String BTC_NXT = "BTC_NXT";
-	public static final String BTC_DGB = "BTC_DGB";
-	public static final String BTC_STRAT = "BTC_STRAT";
-	public static final String BTC_XEM = "BTC_XEM";
-	public static final String USDT_NXT = "USDT_NXT";   
+	public static final int TIME_FRAME_12 = 12;
+	public static final int TIME_FRAME_15 = 15;
+	public static final int TIME_FRAME_20 = 20;
+	public static final int TIME_FRAME_25 = 25;
 					
 }
